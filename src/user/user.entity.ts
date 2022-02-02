2e635c723839986652b5ce9d6b3b14671fea9160
Lyzzi Brooks <lyzzibrooks@Lyzzis-MacBook-Air.local>
2022-02-01 19:13:21 -0600

    columns for user

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -5,6 +5,27 @@ export class User {
   @PrimaryGeneratedColumn()
   id: string;
 
-  @Column({ type: 'varchar', length: 300 })
+  @Column({
+    type: 'varchar',
+    length: 300,
+    nullable: false,
+    unique: true
+  })
   username: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false
+  })
+  password: string;
+
+  @Column({
+      type: 'varchar',
+      nullable: false
+  })
+  email: string;
+  @BeforeInsert() // runs this before saving to db
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 }
