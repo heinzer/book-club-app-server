e8d2e6f4a0265698b0c8b3456fe04b804d167366
Erin Heinz <heinzer@users.noreply.github.com>
2022-02-05 19:16:18 -0600

    Adding back in the profile endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
-import { JwtAuthGuard, Public } from './auth/jwt-auth.guard';
+import { Public } from './auth/jwt-auth.guard';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
@@ -16,5 +16,10 @@ export class AppController {
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
